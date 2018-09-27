8d9c6c48da31a25b32782bb350c5065d9a6a5780
SkyBigBoss03 <Mr_lin_zhang@163.com>
2018-09-27 21:04:59 +0800

    自带简单定时1111111

--- src/main/java/com/chinasoft/springboot/demo/job/JobDemo1.java
+++ src/main/java/com/chinasoft/springboot/demo/job/JobDemo1.java
@@ -8,6 +8,9 @@ import java.util.Date;
 
 @Component
 public class JobDemo1 {
+/*    0 0 10,14,16 * * ? 每天上午10点，下午2点，4点
+    0 0 12 * * ? 每天中午12点触发
+   0 0/5 0 * * ? 每5分钟执行一次*/
     //每分钟执行一次
     @Scheduled(cron = "0 0/1 * * * ?")
     public void testJob(){
